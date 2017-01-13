68084918ab98522d2e1a761df4f5191b462e2ff1
Eleven131792 <lcy802337@163.com>
2017-01-13 14:00:31 +0800

    first commit

---
+++ HeartTrip/Tool/Configure/HTConst.m
@@ -0,0 +1,46 @@
+//
+//  HTConst.m
+//  HeartTrip
+//
+//  Created by 熊彬 on 16/9/13.
+//  Copyright © 2016年 BinBear. All rights reserved.
+//
+
+#import "HTConst.h"
+
+/********** Project Key ***********/
+
+// 首页banner数据key
+NSString *const  BannerDatakey = @"BannerDatakey";
+// 首页热门游记数据key
+NSString *const  TravelDatakey = @"TravelDatakey";
+// 发现视频数据key
+NSString *const  FindVideoDatakey = @"FindVideoDatakey";
+// 发现feed数据key
+NSString *const  FindFeedDatakey = @"FindFeedDatakey";
+// 经度key
+NSString *const  Longitudekey = @"Longitudekey";
+// 纬度key
+NSString *const  Latitudekey = @"Latitudekey";
+// 城市名key
+NSString *const  CityNamekey = @"CityNamekey";
+// 网页标题key
+NSString *const  WebTitlekey = @"WebTitlekey";
+// 网页请求地址key
+NSString *const  RequestURLkey = @"RequestURLkey";
+
+
+/********** 网络请求地址 ***********/
+
+// 服务地址
+NSString *const  HTURL = @"http://api.breadtrip.com";
+NSString *const  HTURL_Test = @"";
+
+
+// 首页
+NSString *const  CityTravel_URL = @"/v2/index/";
+
+// 发现
+NSString *const  Find_URL = @"/hunter/feeds/";
+
+
